a9bbb26b86384664261ca96bbe62fe8af35360de
Nick Booker <NMBooker@gmail.com>
2013-05-02 21:34:05 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NmbTimesheet::Application.config.secret_token = '801d3ae1e3dd3a33e46dd3448ee35f40bd54b4cfc25847062dbb11c560aab36713639883094ce7c49aee53035b657328d0960f7c558e374ac5b90e82b35dd8f5'
