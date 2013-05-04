32b460b4f0319452f1454f962baf3b2b2dab8c9d
Nick Booker <NMBooker@gmail.com>
2013-05-04 12:39:42 +0100

    Add validation to activities and projects

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,11 @@
 class Activity < ActiveRecord::Base
   attr_accessible :code, :name
+
+  validates :name, :presence => true
+  validates :code, :presence => true
+  validates :code, :uniqueness => true
+  validates :code, :format => {
+    :message => 'Only A-Z a-z 0-9 . /  allowed',
+    :with => /\A[A-Za-z0-9.\/]+\z/,
+  }
 end
