b0c2483101d25eb77b78535b6898fe30cbc486d9
Nick Booker <NMBooker@gmail.com>
2013-05-04 13:37:10 +0100

    works index now works quite nicely on mobile.

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,4 +2,7 @@ class Work < ActiveRecord::Base
   attr_accessible :activity_id, :date, :description, :minutes, :project_id
   belongs_to :project
   belongs_to :activity
+
+  default_scope order(' date DESC, created_at DESC, id DESC ')
+
 end
