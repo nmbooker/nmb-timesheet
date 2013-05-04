f42e8b449f1aea75a25a1479fed289fad7a013e7
Nick Booker <NMBooker@gmail.com>
2013-05-04 12:26:07 +0100

    migration for adding activities.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130504111250) do
+ActiveRecord::Schema.define(:version => 20130504112200) do
+
+  create_table "activities", :force => true do |t|
+    t.string   "code"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "projects", :force => true do |t|
     t.string   "code"
