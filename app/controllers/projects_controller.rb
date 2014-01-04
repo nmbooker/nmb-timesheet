d4de949ffe9cb9f4fa5c0d41396d3223da99ed73
Nick Booker <NMBooker@gmail.com>
2014-01-04 20:08:07 +0000

    Change redirect target.  Closes #6

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -46,7 +46,11 @@ class ProjectsController < ApplicationController
 
     respond_to do |format|
       if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
+        format.html {
+          redirect_to action: 'index', flash: {
+            notice: 'Project was successfully created.'
+          }
+        }
         format.json { render json: @project, status: :created, location: @project }
       else
         format.html { render action: "new" }
